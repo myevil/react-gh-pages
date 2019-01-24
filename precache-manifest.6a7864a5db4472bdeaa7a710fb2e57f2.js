54f0d92fbc39329ee1ee7a9b8fe6144d174bce7d
william karno <williamkarno@gmail.com>
2019-01-24 21:14:23 +0700

    Updates

---
+++ precache-manifest.6a7864a5db4472bdeaa7a710fb2e57f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "4992cb73e7eeb7e3a9d9",
+    "url": "/react-gh-pages/static/js/main.4992cb73.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4992cb73e7eeb7e3a9d9",
+    "url": "/react-gh-pages/static/css/main.e75fb88f.chunk.css"
+  },
+  {
+    "revision": "e014e4142aaeb2a954991ee46ef35f42",
+    "url": "/react-gh-pages/index.html"
+  }
+];
